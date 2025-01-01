ec84160ec741e5d769760c13c9c6672f34769943
Manuel Odendahl <wesen@ruinwesen.com>
2025-01-01 12:02:36 -0500

    :sparkles: Further mess with the bleve search

--- cmd/experiment/bleve-search/main.go
+++ cmd/experiment/bleve-search/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/blevesearch/bleve/v2"
 	"github.com/blevesearch/bleve/v2/mapping"
@@ -12,6 +13,8 @@ import (
 	"github.com/go-go-golems/glazed/pkg/doc"
 	"github.com/go-go-golems/glazed/pkg/help"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -40,6 +43,18 @@ type SectionDocument struct {
 
 func (sd SectionDocument) Type() string {
 	return "section"
+}
+
+func init() {
+	// Initialize zerolog with console writer and debug level
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	log.Logger = zerolog.New(consoleWriter).
+		Level(zerolog.DebugLevel).
+		With().
+		Timestamp().
+		Caller().
+		Logger()
 }
 
 // createIndexMapping creates a bleve index mapping for help sections
@@ -86,28 +101,78 @@ func createIndexMapping() mapping.IndexMapping {
 	return indexMapping
 }
 
+// createNewIndex creates a new bleve index with proper logging
+func createNewIndex(indexPath string, mapping mapping.IndexMapping) (bleve.Index, error) {
+	startTime := time.Now()
+	log.Debug().Str("path", indexPath).Msg("Creating new bleve index")
+
+	index, err := bleve.New(indexPath, mapping)
+	if err != nil {
+		log.Error().Err(err).Str("path", indexPath).Msg("Failed to create bleve index")
+		return nil, errors.Wrap(err, "failed to create bleve index")
+	}
+
+	elapsed := time.Since(startTime)
+	log.Debug().
+		Str("path", indexPath).
+		Dur("duration", elapsed).
+		Msg("Successfully created new bleve index")
+
+	return index, nil
+}
+
+// openExistingIndex opens an existing bleve index with proper logging
+func openExistingIndex(indexPath string) (bleve.Index, error) {
+	startTime := time.Now()
+	log.Debug().Str("path", indexPath).Msg("Opening existing bleve index")
+
+	index, err := bleve.Open(indexPath)
+	if err != nil {
+		log.Error().Err(err).Str("path", indexPath).Msg("Failed to open bleve index")
+		return nil, errors.Wrap(err, "failed to open bleve index")
+	}
+
+	elapsed := time.Since(startTime)
+	log.Debug().
+		Str("path", indexPath).
+		Dur("duration", elapsed).
+		Msg("Successfully opened existing bleve index")
+
+	return index, nil
+}
+
 // NewBleveHelpIndex creates a new BleveHelpIndex with the given help system
 func NewBleveHelpIndex(hs *help.HelpSystem, indexPath string) (*BleveHelpIndex, error) {
 	var index bleve.Index
 	var err error
 
-	if _, err := os.Stat(indexPath); os.IsNotExist(err) {
-		// Create new index if it doesn't exist
-		mapping := createIndexMapping()
-		index, err = bleve.New(indexPath, mapping)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to create bleve index")
-		}
-	} else {
-		// Open existing index
-		index, err = bleve.Open(indexPath)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to open bleve index")
-		}
-	}
+	mapping := createIndexMapping()
 
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create/open bleve index")
+	if memoryIndex {
+		log.Debug().Msg("Creating in-memory bleve index")
+		index, err = bleve.NewMemOnly(mapping)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to create in-memory bleve index")
+			return nil, errors.Wrap(err, "failed to create in-memory bleve index")
+		}
+		log.Debug().Msg("Successfully created in-memory bleve index")
+	} else {
+		if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+			// Create new index if it doesn't exist
+			index, err = createNewIndex(indexPath, mapping)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Open existing index
+			index, err = openExistingIndex(indexPath)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &BleveHelpIndex{
@@ -170,10 +235,14 @@ var rootCmd = &cobra.Command{
 	Long:  "A tool to index and search help documentation using the bleve search engine",
 }
 
-var indexPath string
+var (
+	indexPath   string
+	memoryIndex bool
+)
 
 func init() {
 	rootCmd.PersistentFlags().StringVar(&indexPath, "index", "/tmp/bleve-help-index", "Path to the bleve index")
+	rootCmd.PersistentFlags().BoolVar(&memoryIndex, "memory", true, "Create index in memory instead of on disk")
 
 	rootCmd.AddCommand(createIndexCommand())
 	rootCmd.AddCommand(createSearchCommand())
@@ -186,6 +255,8 @@ func createIndexCommand() *cobra.Command {
 		Use:   "index",
 		Short: "Index all help documentation",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			startTime := time.Now()
+
 			// Create help system and load docs
 			helpSystem := help.NewHelpSystem()
 			if err := doc.AddDocToHelpSystem(helpSystem); err != nil {
@@ -214,12 +285,121 @@ func createIndexCommand() *cobra.Command {
 				return errors.Wrap(err, "failed to get document count")
 			}
 
-			fmt.Printf("Successfully indexed %d help sections to %s\n", docCount, indexPath)
+			indexingDuration := time.Since(startTime)
+			fmt.Printf("Successfully indexed %d help sections to %s in %v\n", docCount, indexPath, indexingDuration)
 			return nil
 		},
 	}
 
 	return cmd
+}
+
+// initializeHelpSystem creates and initializes a help system with proper logging
+func initializeHelpSystem() (*help.HelpSystem, error) {
+	log.Debug().Msg("Initializing help system")
+	helpSystem := help.NewHelpSystem()
+	if err := doc.AddDocToHelpSystem(helpSystem); err != nil {
+		log.Error().Err(err).Msg("Failed to load help system")
+		return nil, errors.Wrap(err, "failed to load help system")
+	}
+	log.Debug().Msg("Successfully initialized help system")
+	return helpSystem, nil
+}
+
+// openOrCreateIndex opens an existing index or creates a new one with all sections indexed
+func openOrCreateIndex() (bleve.Index, error) {
+	var index bleve.Index
+	var err error
+
+	// Initialize help system first, outside of timing measurement
+	helpSystem, err := initializeHelpSystem()
+	if err != nil {
+		return nil, err
+	}
+
+	startTime := time.Now()
+	if memoryIndex {
+		log.Debug().Msg("Creating in-memory index")
+		bleveIndex, err := NewBleveHelpIndex(helpSystem, indexPath)
+		if err != nil {
+			return nil, err
+		}
+		index = bleveIndex.index
+	} else {
+		bleveIndex, err := NewBleveHelpIndex(helpSystem, indexPath)
+		if err != nil {
+			return nil, err
+		}
+		index = bleveIndex.index
+	}
+
+	// Index all sections
+	if err := indexAllSections(index, helpSystem); err != nil {
+		return nil, err
+	}
+
+	docCount, err := index.DocCount()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get document count")
+		return nil, errors.Wrap(err, "failed to get document count")
+	}
+
+	indexingDuration := time.Since(startTime)
+	storageType := "disk"
+	if memoryIndex {
+		storageType = "memory"
+	}
+
+	log.Info().
+		Str("type", storageType).
+		Str("path", indexPath).
+		Uint64("documents", docCount).
+		Dur("duration", indexingDuration).
+		Msg("Successfully initialized index")
+
+	return index, nil
+}
+
+// indexAllSections indexes all sections from the help system with proper logging
+func indexAllSections(index bleve.Index, helpSystem *help.HelpSystem) error {
+	log.Debug().Msg("Starting to index all sections")
+	startTime := time.Now()
+
+	batch := index.NewBatch()
+	for _, section := range helpSystem.Sections {
+		doc := SectionDocument{
+			Slug:           section.Slug,
+			Title:          section.Title,
+			SubTitle:       section.SubTitle,
+			Short:          section.Short,
+			Content:        section.Content,
+			Topics:         section.Topics,
+			Flags:          section.Flags,
+			Commands:       section.Commands,
+			SectionType:    section.SectionType.String(),
+			IsTopLevel:     section.IsTopLevel,
+			IsTemplate:     section.IsTemplate,
+			ShowPerDefault: section.ShowPerDefault,
+			Order:          section.Order,
+		}
+		if err := batch.Index(section.Slug, doc); err != nil {
+			log.Error().Err(err).Str("slug", section.Slug).Msg("Failed to index section")
+			return errors.Wrapf(err, "failed to index section %s", section.Slug)
+		}
+	}
+
+	if err := index.Batch(batch); err != nil {
+		log.Error().Err(err).Msg("Failed to execute batch indexing")
+		return errors.Wrap(err, "failed to execute batch indexing")
+	}
+
+	elapsed := time.Since(startTime)
+	log.Debug().
+		Int("sections", len(helpSystem.Sections)).
+		Dur("duration", elapsed).
+		Msg("Successfully indexed all sections")
+
+	return nil
 }
 
 func createSearchCommand() *cobra.Command {
@@ -229,10 +409,10 @@ func createSearchCommand() *cobra.Command {
 		Short: "Search help documentation",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// Open index
-			index, err := bleve.Open(indexPath)
+			// Open or create index
+			index, err := openOrCreateIndex()
 			if err != nil {
-				return errors.Wrap(err, "failed to open index")
+				return err
 			}
 			defer index.Close()
 
@@ -287,18 +467,18 @@ func createFieldSearchCommand() *cobra.Command {
 		Short: "Search help documentation with field filters",
 		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			// Open or create index
+			index, err := openOrCreateIndex()
+			if err != nil {
+				return err
+			}
+			defer index.Close()
+
 			// Validate section type
 			// if sectionType != "" && !validTypes[sectionType] {
 			// 	return fmt.Errorf("invalid section type %q. Valid types are: GeneralTopic, Example, Application, Tutorial", sectionType)
 			// }
 			_ = validTypes
-
-			// Open index
-			index, err := bleve.Open(indexPath)
-			if err != nil {
-				return errors.Wrap(err, "failed to open index")
-			}
-			defer index.Close()
 
 			// Create compound query
 			var queries []query.Query
@@ -366,10 +546,10 @@ func createDebugCommand() *cobra.Command {
 		Use:   "debug",
 		Short: "Show debug information about the index",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// Open index
-			index, err := bleve.Open(indexPath)
+			// Open or create index
+			index, err := openOrCreateIndex()
 			if err != nil {
-				return errors.Wrap(err, "failed to open index")
+				return err
 			}
 			defer index.Close()
 
--- go.mod
+++ go.mod
@@ -9,9 +9,11 @@ require (
 	github.com/a-h/templ v0.2.793
 	github.com/adrg/frontmatter v0.2.0
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de
+	github.com/blevesearch/bleve/v2 v2.4.4
 	github.com/bmatcuk/doublestar/v4 v4.6.1
 	github.com/charmbracelet/glamour v0.7.0
 	github.com/dave/jennifer v1.7.0
+	github.com/gomarkdown/markdown v0.0.0-20241205020045-f7e15b2f3e62
 	github.com/itchyny/gojq v0.12.12
 	github.com/jedib0t/go-pretty v4.3.0+incompatible
 	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54
@@ -41,18 +43,39 @@ require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/RoaringBitmap/roaring v1.9.3 // indirect
 	github.com/alecthomas/chroma/v2 v2.14.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/bits-and-blooms/bitset v1.12.0 // indirect
+	github.com/blevesearch/bleve_index_api v1.1.12 // indirect
+	github.com/blevesearch/geo v0.1.20 // indirect
+	github.com/blevesearch/go-faiss v1.0.24 // indirect
+	github.com/blevesearch/go-porterstemmer v1.0.3 // indirect
+	github.com/blevesearch/gtreap v0.1.1 // indirect
+	github.com/blevesearch/mmap-go v1.0.4 // indirect
+	github.com/blevesearch/scorch_segment_api/v2 v2.2.16 // indirect
+	github.com/blevesearch/segment v0.9.1 // indirect
+	github.com/blevesearch/snowballstem v0.9.0 // indirect
+	github.com/blevesearch/upsidedown_store_api v1.0.2 // indirect
+	github.com/blevesearch/vellum v1.0.10 // indirect
+	github.com/blevesearch/zapx/v11 v11.3.10 // indirect
+	github.com/blevesearch/zapx/v12 v12.3.10 // indirect
+	github.com/blevesearch/zapx/v13 v13.3.10 // indirect
+	github.com/blevesearch/zapx/v14 v14.3.10 // indirect
+	github.com/blevesearch/zapx/v15 v15.3.16 // indirect
+	github.com/blevesearch/zapx/v16 v16.1.9-0.20241217210638-a0519e7caf3b // indirect
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-openapi/errors v0.22.0 // indirect
 	github.com/go-openapi/strfmt v0.23.0 // indirect
-	github.com/gomarkdown/markdown v0.0.0-20241205020045-f7e15b2f3e62 // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -60,6 +83,7 @@ require (
 	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -69,7 +93,10 @@ require (
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/mschoch/smat v0.2.0 // indirect
 	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
@@ -88,6 +115,7 @@ require (
 	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
 	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
 	github.com/yuin/goldmark-emoji v1.0.3 // indirect
+	go.etcd.io/bbolt v1.3.7 // indirect
 	go.mongodb.org/mongo-driver v1.14.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
@@ -97,6 +125,7 @@ require (
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
